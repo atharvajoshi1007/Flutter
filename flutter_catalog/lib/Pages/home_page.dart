150df4279b69ceff9ff74b1d55e5f0bca0484b9f
Atharva Joshi <atharvajoshi1007@gmail.com>
2022-06-12 21:38:39 +0530

    json added

--- flutter_catalog/lib/Pages/home_page.dart
+++ flutter_catalog/lib/Pages/home_page.dart
@@ -1,12 +1,33 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_catalog/models/catolog.dart';
 
 import '../widgets/drawer.dart';
 import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   int days = 30;
+
   String name = "Perceptron";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogjson = await rootBundle.loadString("assets/files/catolog.json");
+    print(catalogjson);
+  }
+
   @override
   Widget build(BuildContext context) {
     final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
