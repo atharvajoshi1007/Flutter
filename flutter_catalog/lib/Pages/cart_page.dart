d33cb79a05c7146ba9ab9c66786d923959d792c5
Atharva Joshi <atharvajoshi1007@gmail.com>
2022-06-13 17:25:10 +0530

    cart list designed

--- flutter_catalog/lib/Pages/cart_page.dart
+++ flutter_catalog/lib/Pages/cart_page.dart
@@ -10,6 +10,58 @@ class CartPage extends StatelessWidget {
         backgroundColor: Colors.green,
         title: "Cart".text.make(),
       ),
+      body: Column(
+        children: [
+          _CartList().p32().expand(),
+          Divider(),
+          _CartTotal(),
+        ],
+      ),
+    );
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          "\Rs9999999".text.xl4.color(Colors.green).make(),
+          30.widthBox,
+          ElevatedButton(
+            onPressed: () {},
+            // style: ButtonStyle(
+            //     backgroundColor:
+            //         MaterialStateProperty.all(context.theme.buttonColor)),
+            child: "Buy".text.make(),
+          ).w24(context),
+        ],
+      ),
+    );
+  }
+}
+
+class _CartList extends StatefulWidget {
+  @override
+  _CartListState createState() => _CartListState();
+}
+
+class _CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) => ListTile(
+        leading: Icon(Icons.done),
+        trailing: IconButton(
+          icon: Icon(Icons.remove_circle_outline),
+          onPressed: () {},
+        ),
+        title: "Item 1".text.make(),
+      ),
     );
   }
 }
